37a8bd5f61b26e2bc8ea64f10dbdca0baacda66d
Dennis Pashkov <dpashkov@provectus.com>
2016-04-14 17:38:13 +0300

    initial commit

---
+++ Source/DPRefreshControl.h
@@ -0,0 +1,19 @@
+//
+//  DPRefreshControl.h
+//  DPRefreshControl
+//
+//  Created by Dennis Pashkov on 4/14/16.
+//  Copyright © 2016 Dennis Pashkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DPRefreshControl.
+FOUNDATION_EXPORT double DPRefreshControlVersionNumber;
+
+//! Project version string for DPRefreshControl.
+FOUNDATION_EXPORT const unsigned char DPRefreshControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DPRefreshControl/PublicHeader.h>
+
+
